88b6000ae90f13dbffa24bdc297e2eee334f7400
Ognjen Cetkovic <ognjen.cetkovic@bitcamp.ba>
2015-09-02 16:08:46 +0200

    Add hash password;

--- bitNavigator/app/models/User.java
+++ bitNavigator/app/models/User.java
@@ -5,18 +5,28 @@ import javax.persistence.*;
 import play.db.ebean.*;
 import play.data.format.*;
 import play.data.validation.*;
+import play.data.validation.Constraints;
+
+import play.Logger;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by ognjen.cetkovic on 01/09/15.
  */
 @Entity
 public class User extends Model {
 
+    public static Finder<Integer, User> finder = new Finder<>(Integer.class, User.class);
+
     @Id
     public int id;
     public String username;
+    @Constraints.Required
     public String email;
     public String firstName;
     public String lastName;
+    @Constraints.Required
     public String password;
 
     public User() {
@@ -28,4 +38,10 @@ public class User extends Model {
         this.password = password;
     }
 
+    public static User findByEmail(String email) {
+        User u = finder.where().eq("email", email).findUnique();
+        //Logger.info(u.email);
+
+        return u;
+    }
 }
