b6ce0307cd83253f45d64a8d5fd44d91bb1847ef
Hajrudin <hajrudin.sehic@bitcamp.ba>
2015-10-18 23:50:11 +0200

    add footer

--- bitNavigator/public/javascripts/footer-script.js
+++ bitNavigator/public/javascripts/footer-script.js
@@ -1,16 +1,16 @@
 $(document).ready(function () {
-    $('.panel-heading').click(function () {
+    $('.panel-heading-footer').click(function () {
         $("i", this).toggleClass("fa fa-arrow-circle-o-up fa fa-arrow-circle-o-down");
     })
 });
 $('#collapseOne').on('show.bs.collapse', function () {
-    $('.panel-heading').animate({
+    $('.panel-heading-footer').animate({
         backgroundColor: "#2b669a"
     }, 500);
 });
 
 $('#collapseOne').on('hide.bs.collapse', function () {
-    $('.panel-heading').animate({
+    $('.panel-heading-footer').animate({
         backgroundColor: "#2b669a"
     }, 500);
 });
